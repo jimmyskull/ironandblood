dc48b0f99c1ff770798d3ead1942aeaf4dc4a8c4
Paulo Roberto Urio <urio@usp.br>
2016-03-27 21:53:33 -0300

    Basic login
    Signed-off-by: Paulo Roberto Urio <urio@usp.br>

--- game/game/settings.py
+++ game/game/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'xix.apps.XixConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'game.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'game.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'NAME': 'ironandblood',
+        'TEST': {
+            'NAME': 'ironandbloodtest',
+        },
     }
 }
 
--- game/xix/views.py
+++ game/xix/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, render_to_response, redirect
+from django.template import RequestContext
+from django.utils.translation import ugettext_lazy as _
 
-# Create your views here.
+@login_required(login_url='login')
+def index(request):
+  return render(request, 'xix/main.html', {})
+
+@login_required(login_url='login')
+def logout_view(request):
+  logout(request)
+  return redirect('login')
+
+def login_view(request):
+  state = _("Please log in above.")
+  alert_type = 'info'
+  username = password = ''
+  if request.POST:
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+      if user.is_active:
+        login(request, user)
+        state = _("You’re successfully logged in!")
+        next = request.GET.get('next', '/')
+        return redirect(next)
+      else:
+        state = _("Your account is not active, please contact the site admin.")
+    else:
+      alert_type = 'danger'
+      state = _("Your username and/or password were incorrect.")
+  context = {'alert_type': alert_type, 'state': state, 'username': username}
+  return render_to_response('xix/auth.html', RequestContext(request, context))
